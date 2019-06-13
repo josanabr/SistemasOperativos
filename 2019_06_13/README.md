c60bf5885c321e6011b77b3efa19b07ced7bd00c
John Sanabria <john.sanabria@gmail.com>
2019-06-13 16:35:51 +0000

    adicionando fork-wait.c

--- 2019_06_13/README.md
+++ 2019_06_13/README.md
@@ -1,3 +1,8 @@
 # Revisión de lectura sobre API en lenguaje C de procesos
 
 * [fork-basico.c](fork-basico.c)
+* [fork-wait.c](fork-wait.c)
+
+# Tarea
+
+* Cómo garantizar que siempre se ejecute primero el padre?
---
+++ 2019_06_13/fork-wait.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <sys/wait.h>
+
+int main(int argc, char** argv) {
+  pid_t pid,pid2;
+  int status;
+
+  pid = fork();
+  if (pid != 0) {
+    pid2 = wait(&status);
+    printf("soy el padre mi id es %d y el de mi hijo es %d\n",getpid(),pid);
+  } else {
+    sleep(3);
+    printf("soy el hijo con id %d y mi variable pid es %d\n",getpid(),pid);
+  }
+
+  return 0;
+}
