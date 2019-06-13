b04b6b9c5c5f60c6809f146c290f0c95b249f61a
John Sanabria <john.sanabria@gmail.com>
2019-06-13 17:00:15 +0000

    adding fibonacci.c

---
+++ 2019_06_13/fibonacci.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+int fibonacci(int n) {
+  if (n == 0) return 0;
+  if (n == 1) return 1;
+  else return fibonacci(n - 1) + fibonacci(n - 2);
+}
+
+int guardarEntero(char *fn, int n) {
+  FILE *fp;
+  fp = fopen(fn, "w");
+  fprintf(fp,"%d",n);
+  fclose(fp);
+  return 0;
+}
+
+int leerEntero(char *fn) {
+  FILE *fp;
+  int n;
+  fp = fopen(fn,"r");
+  fscanf(fp,"%d",&n);
+  fclose(fp);
+  return n;
+}
+
+int main(int argc, char** argv) {
+  int x = 20;
+  guardarEntero("hola.txt",100);
+  x = leerEntero("hola.txt");
+  printf("%d\n",x);
+}
